0ace69c8ddaf0b60256aee13e0406803d489a4e8
Shadi Alostaz <shadialostaz3@gmail.com>
2021-03-16 14:25:03 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,33 @@
+const Discord = require('discord.js')
+const client = new Discord.Client()
+
+client.on('message', async message => {
+
+  if(message.content.toLowerCase() === "?webhook"){
+const fetched = await client.channels.cache.get('SEND TO').messages.fetch({limit: 5});
+client.channels.cache.get('SEND TO').bulkDelete(fetched)
+
+
+const embed = new Discord.MessageEmbed()
+.setTitle('TITLE')
+.setColor('COLOR')
+.setThumbnail('LINK')
+.setDescription(`DESCRIPTION.`)
+.addField('EMBED TITLE', `CONTENT`)
+
+
+
+
+const w = await message.guild.fetchWebhooks()
+const webhook = w.find(c => c.name === "WEBHOOK")
+
+webhook.send({
+  username: "WEBHOOK NAME",
+  avatar: "LINK",
+  embeds: [embed]
+})
+  }
+
+})
+
+
